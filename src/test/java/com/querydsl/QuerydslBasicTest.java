1ff005edec6d2642e24a2cacb42d186dfb8890d0
choidongkuen <danaver12@daum.net>
2023-10-31 17:31:56 +0900

    test : queryDsl 결과 조회 실습

--- src/test/java/com/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
+import static com.querydsl.entity.QMember.member;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -23,22 +26,27 @@ public class QuerydslBasicTest {
     @Autowired
     EntityManager em;
 
+    JPAQueryFactory queryDsl;
+
     /**
      * -- 개별 테스트 실행 전에 실행 되는 메소드
-    **/
+     **/
     @BeforeEach
     public void before() {
+
+        queryDsl = new JPAQueryFactory(em);
+
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
 
         em.persist(teamA);
         em.persist(teamB);
 
-        Member member1 = new Member("member1",10,teamA);
-        Member member2 = new Member("member2",20,teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
 
-        Member member3 = new Member("member3",30,teamB);
-        Member member4 = new Member("member4",40,teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
 
         em.persist(member1);
         em.persist(member2);
@@ -49,7 +57,7 @@ public class QuerydslBasicTest {
 
     @Test
     @DisplayName("JPQL_TEST")
-    void startJPQL(){
+    void startJPQL() {
         Member member = em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", "member1")
                 .getSingleResult();
@@ -60,15 +68,61 @@ public class QuerydslBasicTest {
 
     @Test
     @DisplayName("QueryDsl_TEST")
-    void startQueryDsl(){
-        JPAQueryFactory querydsl = new JPAQueryFactory(em);
-        QMember qMember = QMember.member;
+    void startQueryDsl() {
 
-        Member foundMember = querydsl.selectFrom(qMember)
+        QMember qMember = member;
+
+        Member foundMember = queryDsl.selectFrom(qMember)
                 .where(qMember.name.eq("member1"))
                 .fetchOne();
 
         assertThat(foundMember.getName()).isEqualTo("member1");
         assertThat(foundMember.getAge()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("queryDsl_Search_TEST")
+    void queryDslSearchTest() {
+
+        // eq,ne,in,notin,between,goe,gt,loe,lt,like,contains,startswith
+
+        Member member = queryDsl.selectFrom(QMember.member)
+                .where(QMember.member.name.eq("member1")
+                        .and(QMember.member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(member).isNotNull();
+        assertThat(member.getName()).isEqualTo("member1");
+    }
+
+
+    @Test
+    @DisplayName("queryDsl_search_TEST02")
+    void queryDslSearchTest02() {
+
+        Member member = queryDsl.selectFrom(QMember.member)
+                .where(QMember.member.name.contains("member1"),
+                        QMember.member.age.eq(10))
+                .fetchOne();
+        assertThat(member).isNotNull();
+        assertThat(member.getName()).contains("member1");
+    }
+
+
+    @Test
+    @DisplayName("queryDsl_result_TEST")
+    void queryDslResultTest(){
+
+        List<Member> members = queryDsl.selectFrom(member)
+                .fetch();
+
+        // NonUniqueResultException
+//        Member oneMember = queryDsl.selectFrom(member)
+//                .fetchOne();
+
+        Member firstMember = queryDsl.selectFrom(member)
+                .fetchFirst();
+
+        assertThat(members.size()).isEqualTo(4);
     }
 }
